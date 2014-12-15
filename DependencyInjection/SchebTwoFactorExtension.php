5f4576bc9bf5ae42c88fcdf498a5e7b780f8a158
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-12-15 18:54:28 -0200

    Fixing config

--- DependencyInjection/SchebTwoFactorExtension.php
+++ DependencyInjection/SchebTwoFactorExtension.php
@@ -24,6 +24,7 @@ class SchebTwoFactorExtension extends Extension
         $container->setParameter("scheb_two_factor.email.digits", $config['email']['digits']);
         $container->setParameter("scheb_two_factor.google.server_name", $config['google']['server_name']);
         $container->setParameter("scheb_two_factor.google.template", $config['google']['template']);
+        $container->setParameter("scheb_two_factor.google.use_backup_codes", $config['google']['use_backup_codes']);
         $container->setParameter("scheb_two_factor.trusted_computer.enabled", $config['trusted_computer']['enabled']);
         $container->setParameter("scheb_two_factor.trusted_computer.cookie_name", $config['trusted_computer']['cookie_name']);
         $container->setParameter("scheb_two_factor.trusted_computer.cookie_lifetime", $config['trusted_computer']['cookie_lifetime']);
